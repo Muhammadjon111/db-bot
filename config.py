07878a51ce2d08666ff84d2a3dec240ff4ccb4f2
Muhammadjon111 <muhammadjonkhabibov7@gmail.com>
2026-07-04 09:10:19 +0500

    Config.py modified

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
-# Bismillahir Rohmanir Rohiim
+# # Bismillahir Rohmanir Rohiim
+#
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# TOKEN = os.getenv("TOKEN")
+# DATA_BASE = "database-sample.db"
+# print("TOKEN =", TOKEN)
 
 import os
+
+print("ALL ENV VARS:", os.environ)
 
 TOKEN = os.getenv("TOKEN")
 DATA_BASE = "database-sample.db"
